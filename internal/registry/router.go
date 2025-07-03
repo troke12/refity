f182e209a4473bb80387a58db03480d562e9aee3
I Made Ocy Darma Putra <ochi@troke.id>
2025-07-04 00:22:50 +0700

    fix: remove unused cfg variable in registry router

--- internal/registry/router.go
+++ internal/registry/router.go
@@ -8,12 +8,10 @@ import (
 
 var (
 	ftpClient *ftp.FTPClient
-	cfg       *config.Config
 )
 
 func NewRouterWithDeps(f *ftp.FTPClient, c *config.Config) http.Handler {
 	ftpClient = f
-	cfg = c
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v2/", RegistryHandler)
 	return mux
