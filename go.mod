4ba11ef8ad23b6670c7f8e52b6f940f2aa5a8a8a
I Made Ocy Darma Putra <ochi@troke.id>
2025-07-04 01:31:57 +0700

    refactor: migrate FTP client to SFTP using github.com/pkg/sftp and golang.org/x/crypto/ssh

--- go.mod
+++ go.mod
@@ -1,5 +1,16 @@
 module refity
 
-go 1.21
+go 1.23.0
 
-require github.com/jlaffaye/ftp v0.6.2 
+toolchain go1.23.2
+
+require (
+	github.com/pkg/sftp v1.13.9
+	golang.org/x/crypto v0.39.0
+)
+
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
--- internal/ftp/ftp.go
+++ internal/ftp/ftp.go
@@ -1,31 +1,44 @@
 package ftp
 
 import (
-	"github.com/jlaffaye/ftp"
-	"time"
-	"bytes"
+	"fmt"
 	"io"
+	"log"
+	"os"
 	"path"
 	"strings"
-	"log"
+	"time"
+
+	"golang.org/x/crypto/ssh"
+	"github.com/pkg/sftp"
 )
 
-type FTPClient struct {
-	conn *ftp.ServerConn
+type SFTPClient struct {
+	client *sftp.Client
 }
 
-func NewFTPClient(addr, user, pass string) (*FTPClient, error) {
-	c, err := ftp.Dial(addr, ftp.DialWithTimeout(5*time.Second))
+func NewSFTPClient(host, port, user, pass string) (*SFTPClient, error) {
+	addr := fmt.Sprintf("%s:%s", host, port)
+	config := &ssh.ClientConfig{
+		User: user,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(pass),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout: 10 * time.Second,
+	}
+	conn, err := ssh.Dial("tcp", addr, config)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to dial SSH: %w", err)
 	}
-	if err := c.Login(user, pass); err != nil {
-		return nil, err
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create SFTP client: %w", err)
 	}
-	return &FTPClient{conn: c}, nil
+	return &SFTPClient{client: client}, nil
 }
 
-func (f *FTPClient) ensureDir(dirPath string) error {
+func (f *SFTPClient) ensureDir(dirPath string) error {
 	dirPath = strings.TrimLeft(dirPath, "/")
 	if dirPath == "" || dirPath == "." || dirPath == "/" {
 		return nil
@@ -38,45 +51,53 @@ func (f *FTPClient) ensureDir(dirPath string) error {
 		}
 		current = path.Join(current, p)
 		current = strings.TrimLeft(current, "/")
-		if err := f.conn.MakeDir(current); err != nil && !strings.Contains(err.Error(), "File exists") {
-			log.Printf("ensureDir: failed to create subfolder '%s': %v", current, err)
-			return err
+		if _, err := f.client.Stat(current); os.IsNotExist(err) {
+			if err := f.client.Mkdir(current); err != nil {
+				log.Printf("ensureDir: failed to create subfolder '%s': %v", current, err)
+				return err
+			}
 		}
 	}
 	return nil
 }
 
-func (f *FTPClient) Upload(filePath string, data []byte) error {
+func (f *SFTPClient) Upload(filePath string, data []byte) error {
 	filePath = strings.TrimLeft(filePath, "/")
 	dir := path.Dir(filePath)
 	dir = strings.TrimLeft(dir, "/")
 	if err := f.ensureDir(dir); err != nil {
 		return err
 	}
-	return f.conn.Stor(filePath, bytes.NewReader(data))
+	file, err := f.client.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = file.Write(data)
+	return err
 }
 
-func (f *FTPClient) Download(filePath string) ([]byte, error) {
+func (f *SFTPClient) Download(filePath string) ([]byte, error) {
 	filePath = strings.TrimLeft(filePath, "/")
-	r, err := f.conn.Retr(filePath)
+	file, err := f.client.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
-	defer r.Close()
-	return io.ReadAll(r)
+	defer file.Close()
+	return io.ReadAll(file)
 }
 
-func (f *FTPClient) Close() error {
-	return f.conn.Quit()
+func (f *SFTPClient) List(pathStr string) ([]os.FileInfo, error) {
+	pathStr = strings.TrimLeft(pathStr, "/")
+	return f.client.ReadDir(pathStr)
 }
 
-func (f *FTPClient) List(path string) ([]*ftp.Entry, error) {
-	path = strings.TrimLeft(path, "/")
-	return f.conn.List(path)
-}
-
-func (f *FTPClient) Rename(from, to string) error {
+func (f *SFTPClient) Rename(from, to string) error {
 	from = strings.TrimLeft(from, "/")
 	to = strings.TrimLeft(to, "/")
-	return f.conn.Rename(from, to)
+	return f.client.Rename(from, to)
+}
+
+func (f *SFTPClient) Close() error {
+	return f.client.Close()
 } 
